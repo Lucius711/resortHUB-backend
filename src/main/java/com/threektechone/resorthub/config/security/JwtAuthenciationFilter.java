0956041e9a5974bb96f5b31c9c1ec62876a7039b
Quang Huy <dohuy2915@gmail.com>
2026-05-05 21:39:48 +0700

    feat : filer chain in options method

--- src/main/java/com/threektechone/resorthub/config/security/JwtAuthenciationFilter.java
+++ src/main/java/com/threektechone/resorthub/config/security/JwtAuthenciationFilter.java
@@ -29,6 +29,12 @@ public class JwtAuthenciationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
